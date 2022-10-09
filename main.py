d28cbee1201ebc611ed3cc109c0c4c5feae7761f
Jeremy-Pritt <jfpritt@gmail.com>
2022-10-09 11:52:58 -0600

    hide menu options

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import numpy as np
 import pyodbc
 import streamlit as st
 
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
 st.title("Add Student Databases and Logins")
 
 driver = 'SQL Server'
